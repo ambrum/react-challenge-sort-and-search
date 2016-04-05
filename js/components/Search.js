63c87909d40936c196f689a4ee5b5a2b0a600274
ambrum <irina.ambrumyan@softconstruct.local>
2016-04-05 15:16:14 +0400

    504

--- js/components/Search.js
+++ js/components/Search.js
@@ -5,34 +5,33 @@ class Searchbar extends Component {
 		super(props);
 		this.state={
 			SearchedUser: ''
-		};
-	}
+		}
+	};
 	render(){
-		const duplicatedUserData = _.clone(this.props.UserList);
-		const filtered = [];	
-		onSearch(ev) {
-			this.setState({SearchedUser});
-			const filteredData = duplicatedUserData.map((prop) => {
+		const filtered = [];
+		function onInputSearch(inputValue){
+			console.log(inputValue);
+			this.setState({SearchedUser: inputValue});
+			this.props.UserList.map((prop) => {
 				if(prop.name.match(this.state.SearchedUser)){
 					return filtered.push(prop);
 				}
 			});
-
-		}
-		console.log(filtered);
+			return filtered;
+			
+		};
+		// console.log(filtered);
 		return(
-			<div className="row">
-			<div className="col-sm-12">
-			<input type="text" placeholder="Search people by name..."
-			className="col-lg-12 form-control"
+			<div className = "row">
+			<div className = "col-sm-12">
+			<input type = "text" placeholder = "Search people by name..."
+			className = "col-lg-12 form-control"
 			value = {this.state.SearchedUser}
-			onChange={event => this.onSearch(event.target.value)} />
+			onChange = {event => onInputSearch(event.target.value)} />
 			</div>
 			</div>
 			)
 		}
-		
-
 	}
 
 	export default Searchbar;
